1158643b6e4dfe53ad1fed1540c62931ae9a9d5f
lutsykigor <lutsyk.igor@gmail.com>
2017-01-05 00:12:39 +0100

    Initial commit

---
+++ ExternalMergeSort/LinesComparer.cs
@@ -0,0 +1,46 @@
+﻿namespace ExternalMergeSort
+{
+    using System.Collections.Generic;
+    public class LinesComparer : IComparer<string>
+    {
+        private const char SEPARATOR = '.';
+        private readonly IComparer<string> baseComparer;
+        public LinesComparer(IComparer<string> baseComparer)
+        {
+            this.baseComparer = baseComparer;
+        }
+
+        public int Compare(string x, string y)
+        {
+            var splitX = x.IndexOf(SEPARATOR) + 1;
+            if (splitX == 0)
+            {
+                return 1;
+            }
+
+            var splitY = y.IndexOf(SEPARATOR) + 1;
+            if (splitY == 0)
+            {
+                return -1;
+            }
+
+            var textX = x.Substring(splitX, x.Length - splitX);
+            var textY = y.Substring(splitY, y.Length - splitY);
+
+            var baseRes = baseComparer.Compare(textX, textY);
+
+            if (baseRes == 0)
+            {
+                var numStrX = x.Substring(0, splitX);
+                var numStrY = y.Substring(0, splitY);
+                int numX, numY;
+                if (int.TryParse(numStrX, out numX) && int.TryParse(numStrY, out numY))
+                {
+                    return numX.CompareTo(numY);
+                }
+            }
+
+            return baseRes;
+        }
+    }
+}
---
+++ ExternalMergeSort/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace ExternalMergeSort
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+    using System.IO;
+    using ExternalSort.ExternalMergeSort;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var start = DateTime.Now;
+            // capacity, mergeCount and unsortedFileName are initialized elsewhere
+            var sorter = new TextFileOfNumbersExternalSorter(1000, 100);
+            var sortedFileName = sorter.Sort("<insert-source-file-path-here>");
+
+            var end = DateTime.Now - start;
+            Console.WriteLine(string.Format("Completed. Elapsed {0} seconds", end.TotalSeconds));
+            Console.ReadLine();
+        }
+
+        internal class TextFileOfNumbersExternalSorter : ExternalSorter<string>
+        {
+            public TextFileOfNumbersExternalSorter(int capacity, int mergeCount)
+                : base(new LinesComparer(StringComparer.OrdinalIgnoreCase), capacity, mergeCount)
+            {
+            }
+
+            protected override string Write(IEnumerable<string> run)
+            {
+                var file = Path.GetTempFileName();
+                using (var writer = new StreamWriter(file))
+                {
+                    run.ToList().ForEach(writer.WriteLine);
+                }
+                return file;
+            }
+
+            protected override IEnumerable<string> Read(string name)
+            {
+                using (var reader = new StreamReader(name))
+                {
+                    while (!reader.EndOfStream)
+                        yield return reader.ReadLine();
+                }
+                File.Delete(name);
+            }
+        }
+    }
+}
